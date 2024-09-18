e47fbe56588ee5e1cb3a2398e57d32397a31aecb
José Eduardo <dev.souzaedu@gmail.com>
2024-09-18 10:04:44 -0300

    v2.0 Dominio

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.style.backgroundColor = '#fff';
-        navbar.style.boxShadow = '0 4px 2px -2px gray';
-    } else {
-        navbar.style.backgroundColor = 'transparent';
-        navbar.style.boxShadow = 'none';
-    }
+$(document).ready(function(){
+
+
+    // Navbar sanduíche
+    $('#menu-toggle').click(function() {
+        $('#nav-links').slideToggle();
+    });
 });
