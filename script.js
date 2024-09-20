cb2c40c32dcef9f6fa7018bbff7e69a987d88c66
José Eduardo <dev.souzaedu@gmail.com>
2024-09-20 11:33:11 -0300

    v4.0

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 $(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 
-
-    // Navbar sanduíche
+    // Toggle menu for mobile
     $('#menu-toggle').click(function() {
-        $('#nav-links').slideToggle();
+        $('#nav-links').toggleClass('open');
+    });
+});
+
+// Navbar sanduíche para mobile
+$('#menu-toggle').click(function() {
+    $('#nav-links').slideToggle(); // Usar slideToggle para o efeito dropdown
+});
+
+$(document).ready(function(){
+    // Toggle menu for mobile
+    $('#menu-toggle').click(function() {
+        $('#nav-links').toggleClass('open');
     });
 });
